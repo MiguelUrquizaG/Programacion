6c9e70076d4e99e2149e697211071c36e503c2d6
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-09-25 20:34:03 +0200

    Ejercicio 8

--- UD1EjerList1MiguelUrquiza/src/ejercicio08/Gestion.java
+++ UD1EjerList1MiguelUrquiza/src/ejercicio08/Gestion.java
@@ -6,28 +6,149 @@ public class Gestion {
     private double costeFutbol;
     private double costePelis;
     private double costeSeries;
-    
+    private double descuentoPersonas;
+    private double costePremium;
+    private boolean futbol;
+    private boolean pelis;
+    private boolean series;
+    private double descuentoPaquetes;
 
-
-    public double calcularPVP(int numServicio){
-
+    public Gestion(Plataforma plataforma, double costeFutbol, double costePelis, double costeSeries, double descuentoPersonas, double costePremium) {
+        this.plataforma = plataforma;
+        this.costeFutbol = costeFutbol;
+        this.costePelis = costePelis;
+        this.costeSeries = costeSeries;
+        this.descuentoPersonas = descuentoPersonas;
+        this.costePremium = costePremium;
     }
 
-    public void seleccionarServicio(int num){
 
-        switch (num){
-            case 1:
-                costeSeries =10;
-                break;
+    public Plataforma getPlataforma() {
+        return plataforma;
+    }
 
-            case 2:
-                costeFutbol=20;
-                break;
+    public void setPlataforma(Plataforma plataforma) {
+        this.plataforma = plataforma;
+    }
 
-            default:
-                break;
+    public double getCosteFutbol() {
+        return costeFutbol;
+    }
+
+    public void setCosteFutbol(double costeFutbol) {
+        this.costeFutbol = costeFutbol;
+    }
+
+    public double getCostePelis() {
+        return costePelis;
+    }
+
+    public void setCostePelis(double costePelis) {
+        this.costePelis = costePelis;
+    }
+
+    public double getCosteSeries() {
+        return costeSeries;
+    }
+
+    public void setCosteSeries(double costeSeries) {
+        this.costeSeries = costeSeries;
+    }
+
+    public double getDescuentoPersonas() {
+        return descuentoPersonas;
+    }
+
+    public void setDescuentoPersonas(double descuentoPersonas) {
+        this.descuentoPersonas = descuentoPersonas;
+    }
+
+    public double getCostePremium() {
+        return costePremium;
+    }
+
+    public void setCostePremium(double costePremium) {
+        this.costePremium = costePremium;
+    }
+
+    public boolean isFutbol() {
+        return futbol;
+    }
+
+    public void setFutbol(boolean futbol) {
+        this.futbol = futbol;
+    }
+
+    public boolean isPelis() {
+        return pelis;
+    }
+
+    public void setPelis(boolean pelis) {
+        this.pelis = pelis;
+    }
+
+    public boolean isSeries() {
+        return series;
+    }
+
+    public void setSeries(boolean series) {
+        this.series = series;
+    }
+
+    public double getDescuentoPaquetes() {
+        return descuentoPaquetes;
+    }
+
+    public void setDescuentoPaquetes(double descuentoPaquetes) {
+        this.descuentoPaquetes = descuentoPaquetes;
+    }
+
+    public double calcularPrecio() {
+        double precioFinal = plataforma.getPrecio();
+        if (plataforma.isPremium() && plataforma.getCantidadPersonas() > 3) {
+            precioFinal = precioFinal - descuentoPersonas + costePremium;
+            precioFinal = seleccionarServicio(precioFinal);
+
+        } else if (plataforma.isPremium() && plataforma.getCantidadPersonas() < 3) {
+            precioFinal = precioFinal + costePremium;
+            System.out.println(costePremium);
+            System.out.println(precioFinal);
+            precioFinal = seleccionarServicio(precioFinal);
+            System.out.println("Precio Final" +precioFinal);
+        } else if (!plataforma.isPremium() && plataforma.getCantidadPersonas() > 3) {
+            precioFinal = precioFinal - descuentoPersonas;
+            precioFinal = seleccionarServicio(precioFinal);
         }
 
+
+        return precioFinal;
+    }
+
+
+    public double seleccionarServicio(double precio) {
+
+
+        if (futbol && series && pelis) {
+            descuentoPaquetes = 12;
+            System.out.println(precio);
+            precio+=costePelis+costeFutbol+costeSeries;
+            System.out.println("Paso 1 "+precio);
+            precio -= precio *descuentoPaquetes / 100;
+            System.out.println("Paso 2 "+precio);
+
+        } else if (series && pelis) {
+            descuentoPaquetes = 8;
+            precio+=costePelis+costeSeries;
+            precio -= precio* descuentoPaquetes / 100;
+        } else if (futbol) {
+            precio +=costeFutbol;
+        } else if (series) {
+            precio+=costeSeries;
+        } else if (pelis) {
+            precio+=costePelis;
+        }
+
+        return precio;
 
     }
 
--- UD1EjerList1MiguelUrquiza/src/ejercicio08/Principal.java
+++ UD1EjerList1MiguelUrquiza/src/ejercicio08/Principal.java
@@ -1,8 +1,109 @@
 package ejercicio08;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         //Crea un prorama para gestionar los pagos de plataforma de tv. Decide que atributos, cómo se calculan los distintos precios, por ejemplo, con o sin anuncios, básico o con paquetes
         //extra como el fútbol, descuentos por contratar varias cosas. Intenta que sea lo mas completo posible.
+        Scanner sc =  new Scanner(System.in);
+        int opcion=0, contador=0;
+        Plataforma p = new Plataforma(true,10,"Futflix",2);
+        Gestion g = new Gestion(p,10,20,5,5,10);
+
+
+        do {
+            if(contador>0){
+                System.out.println("¿Desea incorporar/eliminar algún paquete más?");
+                System.out.println("0.No");
+                System.out.println("1.Si");
+                opcion = sc.nextInt();
+            }
+            if(contador ==0 || opcion!=0){
+                System.out.println("Que paquete desea incorporar: ");
+                System.out.println("0.Ninguno");
+                System.out.println("1.Fútbol");
+                System.out.println("2.Series");
+                System.out.println("3.Películas");
+                System.out.println("4.Pack Cinerie (Películas + Series)");
+                System.out.println("5.Pack Full (Fútbol + Películas + Series)");
+                opcion = sc.nextInt();
+
+                //Aqui viene el switch
+                switch(opcion){
+                    case 1:
+                        if(g.isFutbol()){
+                            g.setFutbol(false);
+                            System.out.println("Ha quitado su paquete de fútbol");
+                        }else {
+                            g.setFutbol(true);
+                            System.out.println("Ha añadido su paquete de fútbol, este tiene un coste de: "+g.getCosteFutbol()+"€");
+
+                        }
+                        break;
+                    case 2:
+                        if(g.isSeries()){
+                            g.setSeries(false);
+                            System.out.println("Ha quitado su paquete de series");
+                        }else {
+                            g.setSeries(true);
+                            System.out.println("Ha añadido su paquete de series, este tiene un coste de: "+g.getCosteSeries()+"€");
+
+                        }
+                        break;
+                    case 3:
+                        if(g.isPelis()){
+                            g.setPelis(false);
+                            System.out.println("Ha quitado su paquete de películas");
+                        }else {
+                            g.setPelis(true);
+                            System.out.println("Ha añadido su paquete de películas, este tiene un coste de: "+g.getCostePelis()+"€");
+
+                        }
+                        break;
+                    case 4:
+                        if(!g.isSeries() || !g.isPelis()){
+                            g.setPelis(true);
+                            g.setSeries(true);
+                            System.out.println("Ha contratado el pack Cinerie");
+
+                        }else{
+                            g.setPelis(false);
+                            g.setSeries(false);
+                            System.out.println("Ha retirado el pack Cinerie");
+                        }
+                        break;
+                    case 5:
+                        if(!g.isSeries() || !g.isPelis() || !g.isFutbol()){
+
+                            g.setPelis(true);
+                            g.setSeries(true);
+                            g.setFutbol(true);
+                            System.out.println("Ha contratado el pack Full");
+                            System.out.println("Recibe un descuento del: " +g.getDescuentoPaquetes());
+                        }else{
+                            g.setPelis(false);
+                            g.setSeries(false);
+                            g.setFutbol(false);
+                            System.out.println("Ha retirado el pack Full");
+                        }
+                        break;
+
+                }
+
+                contador++;
+            }
+
+
+        }while(opcion!=0);
+
+
+
+        System.out.println(g.calcularPrecio());
+        if(g.isPelis() && g.isSeries()&&!g.isFutbol()){
+            System.out.println("Al tener pack Cinerie obtienes un descuento del: "+g.getDescuentoPaquetes()+"%");
+        } else if (g.isPelis() && g.isSeries()&&g.isFutbol()) {
+            System.out.println("Al tener pack Full obtienes un descuento del: "+g.getDescuentoPaquetes()+"%");
+        }
     }
 }

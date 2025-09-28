347bfb1f32ae9649039294c547f41963ca8e3835
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-09-28 20:19:25 +0200

    Ejercicio 9

---
+++ UD1EjerList1MiguelUrquiza/src/ejercicio09/Dado.java
@@ -0,0 +1,75 @@
+package ejercicio09;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Dado {
+
+    public int lanzar(int max, int min){
+        Random rnd =  new Random(System.nanoTime());
+
+        return rnd.nextInt(max-min+1)+min;
+    }
+
+
+    public List<Integer> lanzarNumeros(int tiradas,int max, int min){
+
+        List<Integer>numerosAleatorios = new ArrayList<Integer>();
+
+        for(int i=0;i<tiradas;i++){
+
+            numerosAleatorios.add(lanzar(max,min));
+
+        }
+
+        return numerosAleatorios;
+
+    }
+
+    public int buscarNumMasAlto(List<Integer> listaNums){
+        int max=-1;
+        for(int num:listaNums){
+            if(max<num){
+                max =num;
+            }
+        }
+
+        return max;
+    }
+
+    public int buscarNumMasBajo(List<Integer>listaNums){
+        int min=7;
+        for(int num : listaNums){
+            if(min>num){
+                min=num;
+            }
+        }
+        return min;
+
+    }
+
+    public int buscarNumMasRepetido(List<Integer>listaNums){
+
+        int numRepetido=0;
+        int contador1=0;
+        int contador2=0;
+
+        for(int i = 1;i<7;i++){
+
+            for (int a : listaNums){
+                if(a == i){
+                    contador1++;
+                }
+            }
+
+            if(contador1>contador2){
+                contador2=contador1;
+                numRepetido=i;
+            }
+
+        }
+        return numRepetido;
+    }
+
+}
--- UD1EjerList1MiguelUrquiza/src/ejercicio09/Principal.java
+++ UD1EjerList1MiguelUrquiza/src/ejercicio09/Principal.java
@@ -1,8 +1,32 @@
 package ejercicio09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
     public static void main(String[] args) {
-        /* Crea una clase dado con algun metodo como lanzar que devuelva un numero alatorio entra 1 y 6.
-         * Simula 10 tiradas y muestra la puntuacion mayor, la menor y que numero se ha repetido mas veces*/
+        /* Crea una clase Dado con algún método como lanzar() que devuelva un número aleatorio entre 1 y 6. Simula
+10 tiradas y muestra la puntuación mayor, al menor y qué número se ha repetido más veces.
+*/
+
+
+        List<Integer>numeros = new ArrayList<Integer>();
+
+        Dado d  = new Dado();
+
+        numeros = d.lanzarNumeros(10,6,1);
+
+        for(int a : numeros){
+            System.out.println(a);
+        }
+
+        System.out.printf("El número más alto es: %d\n",d.buscarNumMasAlto(numeros));
+        System.out.printf("El número más bajo es: %d\n",d.buscarNumMasBajo(numeros));
+        System.out.printf("El número que mas se ha repetido es: %d",d.buscarNumMasRepetido(numeros));
+
+
+
+
+
     }
 }

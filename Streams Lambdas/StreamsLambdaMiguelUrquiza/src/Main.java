3bcf5ca843896b76a1121941b5a18371a6bb9f88
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-12-18 01:15:25 +0100

    Casa

--- Streams Lambdas/StreamsLambdaMiguelUrquiza/src/Main.java
+++ Streams Lambdas/StreamsLambdaMiguelUrquiza/src/Main.java
@@ -7,12 +7,28 @@ public class Main {
 
     public static void main(String[] args) {
         List<Alumno> alumnoList = List.of(new Alumno("Miguel", "Urquiza", "2ºDAM", 7.32f, 22),
-                new Alumno("Cristina", "Rus", "2ºDAM", 9.12f, 20));
+                new Alumno("Cristina", "Rus", "2ºDAM", 9.12f, 20),
+                new Alumno("Alberto", "Rus", "2ºDAM", 5.12f, 43));
 
 
         Secretaria secretaria = new Secretaria(alumnoList);
 
         secretaria.printAlumnos();
+
+
+        System.out.println(secretaria.alumnoMediaSuperior(7));
+
+        secretaria.printAlumnosNombre("M");
+
+        System.out.println(secretaria.listSize());
+
+        secretaria.imprimirAlumnos(2);
+
+        System.out.println(secretaria.alumnoMenorEdad());
+        System.out.println(secretaria.primerAlumno());
+
+        System.out.println(secretaria.obtenerAlumnosNombre(4));
+        System.out.println(secretaria.obtenerAlumnosLetra(3));
     }
 
 
--- Streams Lambdas/StreamsLambdaMiguelUrquiza/src/models/Secretaria.java
+++ Streams Lambdas/StreamsLambdaMiguelUrquiza/src/models/Secretaria.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Secretaria {
@@ -30,10 +31,54 @@ public class Secretaria {
 
 
     public void printAlumnosNombre(String nombre){
-       /* alumnoList.stream()
-                .filter(a -> a.getNombre())
-                */
+        alumnoList.stream()
+                .filter(a -> a.getNombre().startsWith(nombre))
+                .forEach(System.out::println);
+
     }
+
+    public long listSize(){
+        return alumnoList.stream()
+                .count();
+    }
+
+    public List<Alumno>alumnoMediaSuperior(double media){
+        return alumnoList.stream()
+                .filter(a -> a.getNotaMedia()>media)
+                .toList();
+    }
+
+    public void imprimirAlumnos(int cant){
+        alumnoList.stream()
+                .limit(cant)
+                .forEach(System.out::println);
+    }
+    public Alumno alumnoMenorEdad(){
+        return alumnoList.stream()
+                .min(Comparator.comparingInt(Alumno::getEdad))
+                .orElseThrow();
+    }
+
+    public Alumno primerAlumno(){
+        return alumnoList.stream()
+                .findFirst().orElseThrow();
+    }
+
+    public List<Alumno>obtenerAlumnosNombre(int longitud){
+        return alumnoList.stream()
+                .filter(a-> a.getNombre().length()>longitud)
+                .toList();
+    }
+
+    public List<Alumno>obtenerAlumnosLetra(int longitud){
+        String letra="A";
+        return alumnoList.stream()
+                .filter(a->a.getNombre().startsWith(letra))
+                .filter(a->a.getNombre().length()<=longitud)
+                .toList();
+    }
+
+
 
 
     @Override
